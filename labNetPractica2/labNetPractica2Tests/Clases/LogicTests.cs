4b343e95a41b2ce2137c238828811ec87674cf24
Andres <ANLiendro98@gmail.com>
2023-08-22 22:34:08 -0300

    Commit de Unit Test de las Clases Logic y Operaciones

---
+++ labNetPractica2/labNetPractica2Tests/Clases/LogicTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using labNetPractica2.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica2.Clases.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void LanzarExcepcionTest()
+        {
+            Logic.LanzarExcepcion();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ExcepcionesPersonalizadas))]
+        public void LanzarExcepcionPersonalizadaTest()
+        {
+            Logic.LanzarExcepcionPersonalizada();
+        }
+    }
+}
---
+++ labNetPractica2/labNetPractica2Tests/Clases/OperacionesTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using labNetPractica2.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica2.Clases.Tests
+{
+    [TestClass()]
+    public class OperacionesTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionEj1Test()
+        {
+
+            decimal dividendo = 20;
+            Operaciones.DivisionEj1(dividendo);
+        }
+
+        [TestMethod()]
+        public void DivisionEj2Test()
+        {
+            //Arrange
+            decimal dividendo = 10;
+            decimal divisor = 2;
+            decimal resultadoEsperado = 5;
+
+            //Act
+            decimal resultadoOperacion = Operaciones.DivisionEj2(dividendo, divisor);
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoOperacion);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionEj2NegativoTest()
+        {
+            decimal dividendo = 10;
+            decimal divisor = 0;
+
+            Operaciones.DivisionEj2(dividendo, divisor);
+        }
+    }
+}
