78eb65bf373a57b515f223b89757cb9bc9d7e28d
Andres <ANLiendro98@gmail.com>
2023-09-04 09:19:13 -0300

    Creacion del proyecto en capa

---
+++ labNetPractica4.LINQ/labNetPractica4.LINQ.LOGIC/ContextDb.cs
@@ -0,0 +1,19 @@
+﻿using labNetPractica4.LINQ.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica4.LINQ.LOGIC
+{
+    public class ContextDb
+    {
+        protected NorthwindContext context;
+
+        public ContextDb()
+        {
+            context = new NorthwindContext();
+        }
+    }
+}
---
+++ labNetPractica4.LINQ/labNetPractica4.LINQ.LOGIC/IGet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica4.LINQ.LOGIC
+{
+    public interface IGet<T>
+    {
+        IEnumerable<T> GetConsulta();
+
+    }
+}
