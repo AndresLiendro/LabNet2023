e35a3720b9d1dd03a10e9b720a96582b3031b4af
Andres <ANLiendro98@gmail.com>
2023-09-05 09:10:48 -0300

    Commit arreglo

---
+++ labNetPractica4.LINQ/labNetPractica4.LINQ.UI/Product.UI/ProductsWithoutStockMayMenUI.cs
@@ -0,0 +1,41 @@
+﻿using labNetPractica4.LINQ.LOGIC.Product;
+using labNetPractica4.LINQ.LOGIC.Product.Dtos;
+using labNetPractica4.LINQ.UI.Functionalities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica4.LINQ.UI.Product.UI
+{
+    public class ProductsWithoutStockMayMenUI
+    {
+        public static void ObtenerProductSinStockMayorMenor()
+        {
+            try
+            {
+                AppClean.ClearConsole();
+                ProductWithoutStockMayMen productSinStockMayorMenor = new ProductWithoutStockMayMen();
+                var productSinStockMayorMenorList = productSinStockMayorMenor.GetConsulta();
+                ImprimerProductSinStockMayorMenor(productSinStockMayorMenorList);
+            }
+            catch (Exception ex)
+            {
+                Messages.AppMessages.ExceptionMessage(ex);
+            }
+            finally
+            {
+                Tries.TryAgain();
+            }
+        }
+
+        public static void ImprimerProductSinStockMayorMenor(IEnumerable<ProductsWithoutStockMayMenDto> query)
+        {
+            foreach (var item in query)
+            {
+                Messages.AppMessages.MessageProductsWithoutStockMayMen(item);
+            }
+        }
+    }
+}
