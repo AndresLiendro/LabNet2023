0892b62ab8c76f465befb12235e0c86fc2946a90
Andres <ANLiendro98@gmail.com>
2023-09-05 00:57:42 -0300

    Commit Correxion

--- labNetPractica4.LINQ/labNetPractica4.LINQ.UI/Customer.UI/CustomersWithWARegionUI.cs
+++ labNetPractica4.LINQ/labNetPractica4.LINQ.UI/Customer.UI/CustomersWithWARegionUI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using labNetPractica4.LINQ.ENTITIES;
+using labNetPractica4.LINQ.UI.Functionalities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,34 @@ using System.Threading.Tasks;
 
 namespace labNetPractica4.LINQ.UI.Customer.UI
 {
-    internal class CustomersWithWARegionUI
+    public class CustomersWithWARegionUI
     {
-    }
+		public static void GetCustomersWithWARegion()
+		{
+			try
+			{
+				AppClean.ClearConsole();
+				CustomerFirstThreeRow customerPorRegionWA = new CustomerFirstThreeRow();
+				var customersList = customerPorRegionWA.GetConsulta();
+				ImprimirCustomerConRegionWA(customersList);
+			}
+			catch (Exception ex)
+			{
+
+				Mensajes.MensajeExcepcion(ex);
+			}
+			finally
+			{
+				Intentos.VolverAIntentarlo();
+			}
+		}
+
+		public static void ImprimirCustomerConRegionWA(IEnumerable<Customers> query)
+		{
+			foreach (var item in query)
+			{
+				Mensajes.MensajeCustomerConRegionWA(item);
+			}
+		}
+	}
 }
