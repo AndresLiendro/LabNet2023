60c7e3fbedd4a5199a7347a0f1b10374e01b594d
Andres <ANLiendro98@gmail.com>
2023-09-11 12:25:25 -0300

    Commit de mvc Customer

---
+++ labNetPractica6/labNetPractica6.MVC/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using labNetPractica3.EF.Logic.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labNetPractica6.MVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CustomerServicio customerServicio = new CustomerServicio();
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = customerServicio.GetAll();
+
+            return View(customers);
+        }
+    }
+}
